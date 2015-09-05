56def13ab9ff1e84df710c689702994a66a94557
Vipul Nayyar <nayyar_vipul@yahoo.com>
2015-09-05 20:25:54 +0530

    Multi threading enhancements

--- src/main/python/resources/js/main.js
+++ src/main/python/resources/js/main.js
@@ -1,46 +1,28 @@
-/*
-var obj ="";
-$.get( "api/get_rooms", function( data ) {
-/*	alert( data );
-*//*	
-obj = JSON.parse(JSON.parse(data));
-	console.log(obj);
-
-	var content = "";
-	var count = 0;
-
-	console.log(obj.length + obj[0]);
-
-	for (var i = obj.length - 1; i >= 0; i--) {
-		if(count%3==0) content += "<div class=\"row\">";
-		content += "<div class=\"col-sm-6 col-md-4\"><div class=\"thumbnail\"><img src=\"https://liquidspace.com/Content/Images/SEONew/final-conference-rooms.png\" alt=\"...\"><div class=\"caption\">"; 
-		content += "<h3>" + obj[i]["room_name"]+ "</h3>";
-		content += "<p>"+ obj[i]["room_desc"] +"</p>";
-		content += "<p><a href=\"book_room?room="+ obj[i]["room_id"]+"\" class=\"btn btn-primary\" role=\"button\">Book Room</a></p></div></div>";
-		if (count%3==0) { content += "</div>"; }
-		count++;
-	};
-
-	$("#room_content").html(content);
-});*/
-
-
-
 function get_processes (duration) {
 	
 	$.get( "api/get_processes?duration="+duration, function( data ) {
-		obj = JSON.parse(JSON.parse(data));
+		
+		var	obj = data;
 		console.log(obj);
 		
 		content = "<table class=\"table table-condensed table-striped table-bordered\"><thead><tr> <th> Process ID</th> <th> Received bytes </th> <th> Sent bytes </th> <th> Total Bandwidth </th>  </tr></thead><tbody>";
 		for (var i = obj.length - 1; i >= 0; i--) {
 	
 			content += "<tr>";
-			content += "<td> <a href=\"process/pid\"> " + obj[i]["pid"]+ "</a></td>";
-			content += "<td>" + obj[i]["read"]+ "</td>";
-			content += "<td>" + obj[i]["sent"]+ "</td>";
+			content += "<td> <a href=\"process/"+ obj[i]["pid"]+"\"> " + obj[i]["pid"]+ "</a></td>";
 			
-			var bandwith = obj[i]["read"] + obj[i]["sent"];
+			var read = 0, write = 0;
+
+			for (var j = obj[i]["data"].length - 1; j >= 0; j--) {
+				read += obj[i]["data"][j][1];
+				write += obj[i]["data"][j][2];
+				
+			};
+
+			content += "<td>" + read+ "</td>";
+			content += "<td>" + write+ "</td>";
+			
+			var bandwith = read + write;
 			content += "<td>" + bandwith + "</td>";
 
 			content += "</tr>";
--- src/main/python/watchdog.py
+++ src/main/python/watchdog.py
@@ -11,27 +11,61 @@ import re
 import sqlite3
 import json
 from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
+import MySQLdb
 
 processes = {}
+plock = False
 ret = ""
 
 
 def pid_thread(pid):
     
+    print "started"
+    global plock
     p = subprocess.Popen(str("strace -p " + pid + " ").split(), stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    global processes
     
-    conn = sqlite3.connect('watchdog-'+pid+'.db')
-    conn.execute("CREATE TABLE IF NOT EXISTS 'data' (time INT PRIMARY KEY NOT NULL, read INT, sent INT)")    
-    conn.close()    
+    retcode = p.poll() #returns None while subprocess is running
+    txt = p.stdout.read()
 
+    print " read"
+
+    if txt.find("strace: attach: ptrace(PTRACE_ATTACH, ...): Operation not permitted") >= 0:
+        while plock:
+            plock =True
+
+        processes[pid]["live"] = False
+        processes[pid]["state"] = "remove"
+        processes[pid]["thread"] = ""
+        
+        plock = False
+
+        return
+
+    print "ready"
+
+    db = MySQLdb.connect(host="localhost", # your host, usually localhost
+                     user="root", # your username
+                      passwd="qwerty", # your password
+                      db="watchdog")
+
+    cur = db.cursor()
+    query =  "CREATE TABLE IF NOT EXISTS `" + pid + "` (`time` INT PRIMARY KEY NOT NULL, `read` INT, `sent` INT)"
+    print query
+    cur.execute(query)
+    db.commit()
+    db.close()
+    # conn = sqlite3.connect('watchdog-'+pid+'.db')
+    # conn.execute("CREATE TABLE IF NOT EXISTS 'data' (time INT PRIMARY KEY NOT NULL, read INT, sent INT)")    
+    # conn.close()    
+        
     while(True):
-        conn = sqlite3.connect('watchdog-'+pid+'.db')
-        current_time = int(time.time())
-        retcode = p.poll() #returns None while subprocess is running
-        txt = p.stdout.readline()
         
-        print txt
-        
+        print "in"
+
+        # # print pid 
+        # # print txt
+
         read_bytes = 0
         sent_bytes = 0
 
@@ -67,39 +101,75 @@ def pid_thread(pid):
 
         if(retcode is not None):
             break
-        
-        conn.execute("INSERT INTO 'data' (time,read,sent) VALUES ("+ str(current_time) +", " + str(read_bytes) + ", " + str(sent_bytes) +")")
+       
+        db = MySQLdb.connect(host="localhost", # your host, usually localhost
+                     user="root", # your username
+                      passwd="qwerty", # your password
+                      db="watchdog")
+
+        cur = db.cursor()  
+        current_time = int(time.time())
+        cur.execute("INSERT INTO `" + pid +"` (`time`,`read`,`sent`) VALUES ("+ str(current_time) +", " + str(read_bytes) + ", " + str(sent_bytes) +")")
+        db.commit()
+        db.close()
+
+        # conn = sqlite3.connect('watchdog-'+pid+'.db')        
+        # c = conn.cursor()
+        # # print current_time
+        # c.execute("INSERT INTO 'data' (time,read,sent) VALUES ("+ str(current_time) +", " + str(read_bytes) + ", " + str(sent_bytes) +")")
         print " inserted " + str(current_time) + " " +  str(sent_bytes) + " " + str(read_bytes)
-        conn.close()
+        
+        # conn.commit()
+        # conn.close()
+        
         sleep(1)
+
+        if processes[pid]["live"] == False:
+            return
+
+        retcode = p.poll() #returns None while subprocess is running
+        txt = p.stdout.read()
 
 
 def net_process_monitor():
+
+    global processes
+    global plock
+
     while True:
         net_processes = commands.getoutput("netstat -tap 2> /dev/null | awk '{print $7}' | grep -oE \"[0-9]+\" | uniq") 
         parr = net_processes.split()
         
         current_time = int(time.time())
 
+        # print processes
+        #print parr
         for i in xrange(0,len(parr)-1):
             
+            while plock:
+                plock = True
+
+
             if parr[i] not in processes:
                 processes[parr[i]] = {}
             
-            processes[parr[i]]["live"] = True
-            processes[parr[i]]["thread"] = Thread(target = pid_thread, args = (parr[i],))
-            processes[parr[i]]["entry"] =  current_time
-            processes[parr[i]]["thread"].daemon = True
-            processes[parr[i]]["thread"].start()
+            if "state" in processes[parr[i]] and processes[parr[i]]["state"] == "remove":
+                continue
+
+            if "live" not in processes[parr[i]] or processes[parr[i]]["live"] != True:
+                processes[parr[i]]["thread"] = Thread(target = pid_thread, args = (parr[i],))
+                processes[parr[i]]["entry"] =  current_time
+                processes[parr[i]]["thread"].daemon = True
+                processes[parr[i]]["thread"].start()
+                processes[parr[i]]["live"] = True
 
         for key in processes:
-            if processes[key]["entry"] != current_time:
+            if processes[key]["live"] == True and processes[key]["entry"] != current_time:
                 processes[key]["live"] = False
                 processes[key]["thread"] = ""
 
-        #print processes
-
         #sleep(2)
+        plock = False
 
 class myHandler(BaseHTTPRequestHandler):
     
@@ -108,29 +178,89 @@ class myHandler(BaseHTTPRequestHandler):
         temp = re.search("/api/get_processes\?duration=([0-9]+)", self.path) 
         print self.path
 
+        global processes
+
         if temp != None:
             
             print "api processes"
             duration = temp.group(1)
             start_time = int(time.time()) - int(duration)
-            print start_time
+            #print start_time
 
             self.send_response(200)
             self.send_header('Content-type',"text/json")
             self.end_headers()
 
             send = []
+
+            #print processes
             
             for key in processes:
+                #print "key"
 
                 if processes[key]["live"] == False:
                     continue
-                conn = sqlite3.connect('watchdog-'+key+'.db')
-                c = conn.cursor()
+                print "con"
+                db = MySQLdb.connect(host="localhost", # your host, usually localhost
+                     user="root", # your username
+                      passwd="qwerty", # your password
+                      db="watchdog")
+
+                cur = db.cursor()  
+                current_time = int(time.time())
+                cur.execute('SELECT * FROM data WHERE time >= (?)', (str(start_time),))
+                db.commit()
+                db.close()
+
+                # conn = sqlite3.connect('watchdog-'+key+'.db')
+                # c = conn.cursor()
                 # c.execute('SELECT * FROM data WHERE time >= (?)', (str(start_time),))
-                c.execute('SELECT * FROM data')
-                temp_data = c.fetchall()
-                print temp_data
+                #c.execute('SELECT * FROM data')
+                temp_data = cur.fetchall()
+                # print temp_data
+                temp_obj = {}
+                temp_obj["pid"] = key
+                temp_obj["data"] = temp_data
+                send.append(temp_obj)
+                # conn.close()
+
+            print send
+
+            self.wfile.write(json.dumps(send))
+            return
+
+        temp = re.search("/process/([0-9]+).*", self.path) 
+        print self.path
+
+        if temp != None:
+            
+            print "process page"
+            pid = temp.group(1)
+
+            temp = re.search("\?duration=([0-9]+)", self.path) 
+
+            start_time = -1
+            if temp!=None:
+                duration = temp.group(1)
+                start_time = int(time.time()) - int(duration)
+            
+            self.send_response(200)
+            self.send_header('Content-type',"text/json")
+            self.end_headers()
+
+            send = []
+            
+            conn = sqlite3.connect('watchdog-'+pid+'.db')
+            c = conn.cursor()
+
+            if start_time == -1:
+                start_time = int(time.time()) - 86400
+
+            c.execute('SELECT * FROM data WHERE time >= (?)', (str(start_time),))
+            # c.execute('SELECT * FROM data')
+            
+            temp_data = c.fetchall()
+            print temp_data
 
             print send
 
@@ -188,21 +318,16 @@ class watchdog:
         try:
             thread = Thread(target = net_process_monitor)
             thread.start()
-        except KeyboardInterrupt:
-            print "vdf"
-            thread.stop()
-            sys.exit(0)
-
-        try:
-        
-            server = HTTPServer(('', 8998), myHandler)
+            server = HTTPServer(('', 9888), myHandler)
             print server
-            server.serve_forever()
-        
+            thread2= Thread(target = server.serve_forever)
+            thread2.start()
+            print "stop"
         except KeyboardInterrupt:
-
+            print "KeyboardInterrupt"
+            #thread.stop()
             server.socket.close()
-
+            sys.exit(0)
 
 
 
